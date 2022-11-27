056090becd42a65bf16bd368b17f5940a8e452d0
nils <nilsjaekel16@gmail.com>
2022-11-27 14:00:09 +0100

    add translate command

---
+++ commands/translator/translate.py
@@ -0,0 +1,27 @@
+import nextcord
+import nextcord.ext
+from nextcord.ext import commands
+from deep_translator import GoogleTranslator
+
+
+class Translate(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="nyro-translate",
+        description="Translate any text to any language",
+        force_global=True
+    )
+    async def translate(self, ctx: nextcord.Interaction, language: str, text: str):
+        if language not in GoogleTranslator().get_supported_languages():
+            return await ctx.send("Language not supported.")
+
+        translator = GoogleTranslator(target=language)
+        translated_text = translator.translate(text=text)
+
+        await ctx.send(translated_text, ephemeral=True)
+
+
+def setup(bot):
+    bot.add_cog(Translate(bot))
