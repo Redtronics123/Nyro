c528d523e77461ff5ec042089b85a1d722b4cb98
nils <nilsjaekel16@gmail.com>
2022-12-03 13:07:58 +0100

    Add select menu to translator

--- commands/translator/translate.py
+++ commands/translator/translate.py
@@ -1,6 +1,7 @@
 import nextcord.ext
 from nextcord.ext import commands
 from deep_translator import GoogleTranslator
+from template import string_select
 
 
 class Translate(commands.Cog):
@@ -12,11 +13,20 @@ class Translate(commands.Cog):
         description="Translate any text to any language",
         force_global=True
     )
-    async def translate(self, ctx: nextcord.Interaction, language: str, text: str):
-        if language not in GoogleTranslator().get_supported_languages():
-            return await ctx.send("Language not supported.")
+    async def translate(self, ctx: nextcord.Interaction, text: str):
+        translator_view = string_select.TemplateStringSelect(
+            ctx,
+            ["German", "English", "Danish", "Finnish", "French", "Indonesian", "Irish", "Italian", "Latin"],
+            placeholder="Select a language"
+        )
+        await ctx.send(view=translator_view, ephemeral=True)
+        await translator_view.wait()
 
-        translator = GoogleTranslator(target=language)
+        try:
+            translator = GoogleTranslator(target=str(translator_view.select.values[0]).lower())
+        except IndexError:
+            return await ctx.send("No language selected.", ephemeral=True)
+
         translated_text = translator.translate(text=text)
 
         await ctx.send(translated_text, ephemeral=True)
